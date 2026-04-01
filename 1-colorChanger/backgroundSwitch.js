9ff5706854985ce7e5815d23751fd1e7d4cb7885
PriyanshuN-11 <negip5489@gmail.com>
2026-04-01 09:11:23 +0530

    background changer

--- 1-colorChanger/backgroundSwitch.js
+++ 1-colorChanger/backgroundSwitch.js
@@ -1,7 +1,39 @@
-/*
-function
-Dom
-events
-iteration(loop)
-conditional (if else)
-*/
+// const webBody = document.querySelector("body")
+// const webButton = document.getElementsByClassName("button")
+// const arrWebButton = Array.from(webButton)
+
+// arrWebButton.forEach(function (buttonChange){
+//     buttonChange.addEventListener('click', function (buttonEvent){
+//      webBody.style.backgroundColor = buttonEvent.target.id 
+//     })
+// })
+
+const webBody = document.querySelector("body");
+const webButton = document.querySelectorAll(".button");
+
+webButton.forEach(function (singleButton) {
+  singleButton.addEventListener('click', function (clickbutton) {
+    const color = clickbutton.target.id;
+
+    switch (color) {
+      case "red":
+        webBody.style.backgroundColor = color;
+        break;
+
+      case "green":
+        webBody.style.backgroundColor = color;
+        break;
+
+      case "blue":
+        webBody.style.backgroundColor = color;
+        break;
+
+      case "yellow":
+        webBody.style.backgroundColor = color;
+        break;
+
+      default:
+        console.log("Unknown color");
+    }
+  });
+});
